4eb11584df0c3fe6097a934e32eb540535f5a494
Lucas Bos <lucas@minox.nl>
2020-12-11 08:37:22 +0100

    Finished day 11

---
+++ day_11/part_2.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+(() => {
+    const dataset = fs.readFileSync("./dataset.txt").toString();
+    const areas = dataset.split("\n").map((e) => e.split(""));
+
+    const calculateOccupiedSeats = (original_areas) => {
+        let total_occupied_seats = 0;
+        const new_areas = original_areas.map((e) => [...e]);
+        for (let i = 0; i < original_areas.length; i += 1) {
+            for (let j = 0; j < original_areas[i].length; j += 1) {
+                const current_spot = original_areas[i][j];
+                let occupied_seats_around = 0;
+                if (current_spot === ".") continue;
+                if (current_spot === "#") total_occupied_seats += 1;
+                for (let k = -1; k <= 1; k += 1) {
+                    for (let l = -1; l <= 1; l += 1) {
+                        if (k === 0 && l === 0) continue;
+                        let found_seat;
+                        let x = i + k;
+                        let y = j + l;
+                        while (
+                            !found_seat &&
+                            x >= 0 &&
+                            x < original_areas.length &&
+                            y >= 0 &&
+                            y < original_areas[i].length
+                        ) {
+                            if (original_areas[x]?.[y] !== ".") {
+                                found_seat = original_areas[x][y];
+                            } else {
+                                x += k;
+                                y += l;
+                            }
+                        }
+                        if (found_seat === "#") {
+                            occupied_seats_around += 1;
+                        }
+                    }
+                }
+                if (current_spot === "L" && occupied_seats_around === 0) {
+                    new_areas[i][j] = "#";
+                    total_occupied_seats += 1;
+                } else if (current_spot === "#" && occupied_seats_around >= 5) {
+                    new_areas[i][j] = "L";
+                    total_occupied_seats -= 1;
+                }
+            }
+        }
+        return [total_occupied_seats, new_areas];
+    };
+
+    let seats;
+    let new_areas = areas;
+    while (true) {
+        const old_seats = seats;
+        [seats, new_areas] = calculateOccupiedSeats(new_areas);
+        if (old_seats === seats) break;
+    }
+    console.log("🚶‍♀️", seats);
+})();
